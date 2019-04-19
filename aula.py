c46c5a4b284121afe666bb69a1d04c4a3455cc27
Dieinimy Maganha <dieinimy@gmail.com>
2019-04-19 14:45:49 -0300

    Incluido nome nos Eixos

--- aula.py
+++ aula.py
@@ -1,7 +1,15 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2]
-y = [2, 3]
+x = [1, 2, 5]
+y = [2, 3, 7]
+
+# Titulo
+plt.title("Meu primeiro gráfico com Python")
+
+# Eixos
+
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
 
 plt.plot(x, y)
 plt.show()
